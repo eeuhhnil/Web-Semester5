36ca55f5cdb6a545567d97bff0fdcb3ecbacb55e
eeuhhnil <144205221+eeuhhnil@users.noreply.github.com>
2023-11-20 20:07:54 +0300

    fix: js

--- assets/js/shipper.js
+++ assets/js/shipper.js
@@ -1,76 +1,44 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", async function(){
     let userApi = 'https://jsonplaceholder.typicode.com/users';
     const container = document.getElementById("shipper-infor")
+    const template = this.document.getElementById("shipper-template")
     let users_list = []
     function getRandomElement(array) {
-    const shuffled = array.sort(() => 0.5 - Math.random());
+    const copyArray = [...array]
+    const shuffled = copyArray.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, Math.floor(Math.random() * 10));
     }
 
-    fetch(userApi)
-        .then(function (response){
-         return response.json();
-        })
-        .then(function (users){
-            users_list = getRandomElement(users);
-            if (users_list.length === 0) {
-                const message = document.createElement("p")
-                message.innerHTML = "There is not free shipper now!"
-                message.classList.add("shipper-information__shipper-not-available-message")
-                container.appendChild(message)
-                return
-            }
+    try {
+        const response = await fetch(userApi);
+        users_list = getRandomElement(await response.json());
 
-            users_list.forEach(user => {
-                const shipper_card = document.createElement("div");
-                const name = document.createElement("div");
-                const id = document.createElement("div");
-                const phone = document.createElement("div");
-                const email = document.createElement("div");
-                const address = document.createElement("div");
+        if (users_list.length === 0) {
+            const message = document.createElement("p");
+            message.innerHTML = "There is no free shipper now!";
+            message.classList.add("shipper-information__shipper-not-available-message");
+            container.appendChild(message);
+            return;
+        }
 
-                id.innerHTML = 'ID:\t' + user.id;
-                name.innerHTML = user.name;
-                phone.innerHTML = 'Phone:\t' + user.phone;
-                email.innerHTML = 'Email:\t' + user.email;
-                address.innerHTML = 'Address:\t' + user.address.zipcode + ', ' + user.address.street + ', ' + user.address.city;
+        users_list.forEach(user => {
+            const shipper_card = document.importNode(template.content, true);
 
-            
-                shipper_card.classList.add("shipper-information__shipper-card")
-                id.classList.add("shipper-information__shipper-card__id")
-                name.classList.add("shipper-information_shipper-card_name")
-                phone.classList.add("shipper-information_shipper-card_phone")
-                email.classList.add("shipper-information_shipper-card_email")
-                address.classList.add("shipper-information_shipper-card_address")
+            shipper_card.querySelector(".shipper-information__shipper-card__id").innerHTML = 'ID:\t' + user.id;
+            shipper_card.querySelector(".shipper-information_shipper-card_name").innerHTML = user.name;
+            shipper_card.querySelector(".shipper-information_shipper-card_phone").innerHTML = 'Phone:\t' + user.phone;
+            shipper_card.querySelector(".shipper-information_shipper-card_email").innerHTML = 'Email:\t' + user.email;
+            shipper_card.querySelector(".shipper-information_shipper-card_address").innerHTML = 'Address:\t' + user.address.zipcode + ', ' + user.address.street + ', ' + user.address.city;
 
-                id.classList.add("shipper-information__shipper-card__element")
-                name.classList.add("shipper-information__shipper-card__element")
-                phone.classList.add("shipper-information__shipper-card__element")
-                email.classList.add("shipper-information__shipper-card__element")
-                address.classList.add("shipper-information__shipper-card__element")
-
-                shipper_card.appendChild(name)
-                shipper_card.appendChild(id)
-                shipper_card.appendChild(phone)
-                shipper_card.appendChild(email)
-                shipper_card.appendChild(address)
-                container.appendChild(shipper_card)
-            })
-
-        
-        // console.log(users_list);
-        })
-        .catch(function(error){
-            const message = document.createElement("p")
-            message.innerHTML = "Cannot load page"
-            message.classList.add("error_message")
-            container.appendChild(message)
-        })
-        .finally(
-            document.getElementsByClassName("preloader")[0].style.display = "none"
-        )
+            container.appendChild(shipper_card);
+        });
+    } catch (error) {
+        const message = document.createElement("p");
+        message.innerHTML = "Cannot load page";
+        message.classList.add("error_message");
+        container.appendChild(message);
+    } finally {
+        document.getElementsByClassName("preloader")[0].style.display = "none";
+    }
 
     })
-
-
-
